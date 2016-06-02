dd1523ee1fe999bc9ba5687491a40b0571388ea1
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2016-06-02 14:36:58 +0300

    Learning params, random init

--- main.m
+++ main.m
@@ -3,6 +3,7 @@
 % author: Evgeniy Kuznetsov
 %
 
+% Load
 disp("Load data");
 
 loadData();
@@ -11,6 +12,8 @@ load ./jester-11.mat;
 
 fprintf("Loaded %i users with %i reviews\n\n", size(jesterData1));
 
+
+% Process
 disp("Process data");
 
 disp("Before:");
@@ -20,4 +23,21 @@ data = processData(jesterData1);
 
 disp("After:");
 disp(data(1:5,1:5));
+
+
+% Learn
+disp("Learning");
+
+num_features = 10;
+num_users = size(data)(2);
+num_jokes = size(data)(1);
+X = randn(num_jokes, num_features);
+Theta = randn(num_users, num_features);
+R = data != 0;
+
+init_params = [X(:); Theta(:)];
+
+opts = optimset('GradObj', 'on', 'MaxIter', 40);
+
+% learned_params = fminunc();
 

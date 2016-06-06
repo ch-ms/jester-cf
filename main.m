7e4c5493cf457927827373f755b3667409ba97a7
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2016-06-06 17:46:46 +0300

    Max/min/mean validation, validation for random user

--- main.m
+++ main.m
@@ -49,3 +49,48 @@ endif
 
 J = calcCost(X, Theta, Y, R);
 fprintf("Learned, cost=%i.\n\n", J);
+
+
+% Validation
+disp("Validation.");
+
+% Max diff
+predictions = (X * Theta' - Y) .* R;
+predictions = predictions(find(predictions != 0));
+predictions = abs(predictions / 20);
+max_diff = max(predictions(:));
+mean_diff = 1/length(predictions(:)) * sum(predictions(:));
+min_diff = min(predictions(:));
+
+fprintf("Max/min/mean diff %i %i %i.\n", max_diff, min_diff, mean_diff);
+disp("Press any key to continue");
+pause;
+
+% Select random user
+user_id = round(rand() * num_users - 1) + 1;
+user_ratings = Y(:, user_id);
+user_rated_ids = find(user_ratings != 0);
+user_rated = user_ratings(user_rated_ids);
+user_predictions = X * Theta(user_id, :)';
+user_predictions = user_predictions(user_rated_ids);
+user_predictions = round(user_predictions);
+diff = user_rated - user_predictions;
+diff_p = abs(diff / 20);
+max_diff_p = max(diff_p);
+
+fprintf("User is %i.\n\n", user_id);
+fprintf("Predictions:\n");
+
+disp("R/P/D/Dp");
+for i = 1:length(user_rated)
+  fprintf("%i %i %i %i\n", user_rated(i), ...
+    user_predictions(i), diff(i), diff_p(i));
+endfor
+
+fprintf("\nMax diff is %i.\n\n", max_diff_p);
+
+
+% Predictions
+
+
+
